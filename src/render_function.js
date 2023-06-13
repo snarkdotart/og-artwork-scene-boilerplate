bb3c94625713a8219e432e748b6a3e219e20fb7e
paulwinex <paulwinex@gmail.com>
2023-06-13 19:15:11 +0300

    OG-3453 updated default render function

--- src/render_function.js
+++ src/render_function.js
@@ -1,3 +1,24 @@
-function render_function(data) {
-    console.log('render_function.js', data);
+function render_function(data, canvas) {
+    let ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = true;
+    let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+    gradient.addColorStop(0, getRandomColor());
+    gradient.addColorStop(1, getRandomColor());
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    let font_size = data.font_size || 50;
+    ctx.font = `${font_size}px Arial`;
+    ctx.fillStyle = data.text_color || "#FFF";
+    let text = data.text || 'Example Text';
+    let textWidth = ctx.measureText(text).width;
+    let x = (canvas.width - textWidth) / 2;
+    let y = canvas.height / 2;
+    ctx.fillText(text, x, y);
+    // on render completed
+    render_completed()
+}
+
+function getRandomColor() {
+  const hue = Math.floor(Math.random() * 361);
+  return  `hsl(${hue}, 100%, 50%)`;
 }
