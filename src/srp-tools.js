985d956957a643c9afa1f34016086d2b1d5e7d79
Yuriy Kaygorodov <gaiverrr@gmail.com>
2023-06-14 15:37:30 +0100

    add vite configuration

--- src/srp-tools.js
+++ src/srp-tools.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 const get_url_parameters = () => {
     const params = new URLSearchParams(window.location.search);
     return Object.fromEntries(params.entries());
@@ -7,31 +5,30 @@ const get_url_parameters = () => {
 
 const execute = (render_function) =>  {
     let params = get_url_parameters()
-    $.ajax({
-        url: params.input_data || 'http://localhost:9000/input_data',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
+    fetch(params.input_data || 'http://localhost:9000/input_data')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
             // get canvas element
             let canvas = document.getElementById("render");
             // set canvas size
             let resolution = data.resolution || [1920, 1080]
             console.log('Resolution: ', resolution)
             // set canvas size
-            $(canvas).attr({
-                "width": `${resolution[0]}`,
-                "height": `${resolution[1]}`
-            }).css({
-                "width": `${resolution[0]}px`,
-                "height": `${resolution[1]}px`
-            });
+            canvas.setAttribute("width", `${resolution[0]}`);
+            canvas.setAttribute("height", `${resolution[1]}`);
+            canvas.style.width = `${resolution[0]}px`;
+            canvas.style.height = `${resolution[1]}px`;
             // start rendering
-            render_function(data, canvas)
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error(textStatus, errorThrown);
-        }
-    });
+            render_function(data, canvas);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
 const render_completed = () => {
