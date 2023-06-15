c9e39b85011cd2fb67c145c4b894aff350e4ebb1
paulwinex <paulwinex@gmail.com>
2023-06-15 15:03:53 +0300

    added debug server for serving json data

---
+++ debug-server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Debug server for testing data.
+1. Start server.
+2. Open browser and go to: http://localhost:9000
+3. Edit data in debug.json file to change input data for generative scene.
+"""
+import json
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from pathlib import Path
+
+
+DEFAULT_DATA = {'text': 'example'}
+DEBUG_FILE = Path(os.getenv('DEBUG_FILE', 'debug.json'))
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(json.dumps(self.get_debug_data()).encode())
+
+    def get_debug_data(self):
+        if not DEBUG_FILE.exists():
+            DEBUG_FILE.write_text(json.dumps(DEFAULT_DATA))
+            print('Created debug json file:', DEBUG_FILE.as_posix())
+        with DEBUG_FILE.open() as f:
+            return json.load(f)
+
+
+if __name__ == '__main__':
+    host = os.getenv('DEBUG_HOST', 'localhost')
+    port = int(os.getenv('DEBUG_PORT', 9000))
+    httpd = HTTPServer((host, port), SimpleHTTPRequestHandler)
+    print(f'Server started at http://{host}:{port}')
+    print('Debug data file:', DEBUG_FILE.as_posix())
+    httpd.serve_forever()
