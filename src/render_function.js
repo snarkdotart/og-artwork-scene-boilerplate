08607a10b180d008dc447bb0361240abfdaafbc4
paulwinex <paulwinex@gmail.com>
2023-06-16 17:38:16 +0300

    make debug render in default render function

--- src/render_function.js
+++ src/render_function.js
@@ -2,25 +2,28 @@ const getRandomColor = () => {
     const hue = Math.floor(Math.random() * 361);
     return `hsl(${hue}, 100%, 50%)`;
 };
+
+
 const render_function = (data, canvas) => {
-    let ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d');
+    // draw bg
     ctx.imageSmoothingEnabled = true;
     let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
     gradient.addColorStop(0, getRandomColor());
     gradient.addColorStop(1, getRandomColor());
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    let font_size = data.font_size || 50;
-    ctx.font = `${font_size}px Arial`;
-    ctx.fillStyle = data.text_color || "#FFF";
-    let text = data.text || 'Example Text';
-    let textWidth = ctx.measureText(text).width;
-    let x = (canvas.width - textWidth) / 2;
-    let y = canvas.height / 2;
-    ctx.fillText(text, x, y);
-    // on render completed
+    // draw text
+    ctx.fillStyle = 'black';
+    ctx.font = '15px monospace';
+    const text = JSON.stringify(data, null, 2);
+    var lines = text.split('\n');
+
+    for (var i = 0; i < lines.length; i++) {
+      ctx.fillText(lines[i], 10, 20 + (i * 20));
+    }
     render_completed()
-};
+}
 
 export {
     render_function
