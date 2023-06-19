682a17fdf52ffdd54b586ab408750e032b63e485
Yuriy Kaygorodov <gaiverrr@gmail.com>
2023-06-19 13:59:20 +0100

    feature/og-3463-artwork-generator-boilerplate-is-blank

--- main.js
+++ main.js
@@ -1,46 +1,62 @@
-import './style.css'
-import {render_function} from "./src/render_function.js";
+import "./style.css";
+import { render_function } from "./src/render_function.js";
+import exampleData from "./example.json";
 
 const render_completed = () => {
-    window.isAnimated = true;
-    document.isAnimated = true;
+  window.isAnimated = true;
+  document.isAnimated = true;
 };
 
 window.render_completed = render_completed;
 
 const get_url_parameters = () => {
-    const params = new URLSearchParams(window.location.search);
-    return Object.fromEntries(params.entries());
+  const params = new URLSearchParams(window.location.search);
+  return Object.fromEntries(params.entries());
 };
 
-const execute = (render) =>  {
-    let params = get_url_parameters()
-    fetch(params.input_data || 'http://0.0.0.0:9000/input_data')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            // get canvas element
-            let canvas = document.getElementById("render");
-            // set canvas size
-            let resolution = data.resolution || [1920, 1080]
-            console.log('Set Resolution: ', resolution)
-            canvas.setAttribute("width", `${resolution[0]}`);
-            canvas.setAttribute("height", `${resolution[1]}`);
-            canvas.style.width = `${resolution[0]}px`;
-            canvas.style.height = `${resolution[1]}px`;
-            // start rendering
-            render(data, canvas);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+const execute = (render) => {
+  let params = get_url_parameters();
+  const url = params.input_data
+    ? params.input_data
+    : import.meta.env.VITE_BACKEND_URL
+    ? import.meta.env.VITE_BACKEND_URL + "/input_data"
+    : null;
 
-document.addEventListener('DOMContentLoaded', function() {
-    execute(render_function)
-}, false);
+  const handleResponse = (data) => {
+    // get canvas element
+    let canvas = document.getElementById("render");
+    // set canvas size
+    let resolution = data.resolution || [1920, 1080];
+    console.log("Set Resolution: ", resolution);
+    canvas.setAttribute("width", `${resolution[0]}`);
+    canvas.setAttribute("height", `${resolution[1]}`);
+    canvas.style.width = `${resolution[0]}px`;
+    canvas.style.height = `${resolution[1]}px`;
+    // start rendering
+    render(data, canvas);
+  };
 
+  if (url) {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(handleResponse)
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    handleResponse(exampleData);
+  }
+};
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    execute(render_function);
+  },
+  false
+);
--- src/render_function.js
+++ src/render_function.js
@@ -1,30 +1,29 @@
 const getRandomColor = () => {
-    const hue = Math.floor(Math.random() * 361);
-    return `hsl(${hue}, 100%, 50%)`;
+  const hue = Math.floor(Math.random() * 361);
+  return `hsl(${hue}, 100%, 50%)`;
 };
 
-
 const render_function = (data, canvas) => {
-    const ctx = canvas.getContext('2d');
-    // draw bg
-    ctx.imageSmoothingEnabled = true;
-    let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-    gradient.addColorStop(0, getRandomColor());
-    gradient.addColorStop(1, getRandomColor());
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // draw text
-    ctx.fillStyle = 'black';
-    ctx.font = '15px monospace';
-    const text = JSON.stringify(data, null, 2);
-    var lines = text.split('\n');
+  const ctx = canvas.getContext("2d");
 
-    for (var i = 0; i < lines.length; i++) {
-      ctx.fillText(lines[i], 10, 20 + (i * 20));
-    }
-    window.render_completed()
-}
+  // draw bg
+  ctx.imageSmoothingEnabled = true;
+  let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+  gradient.addColorStop(0, getRandomColor());
+  gradient.addColorStop(1, getRandomColor());
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // draw text
+  ctx.fillStyle = "black";
+  ctx.font = "15px monospace";
+  const text = JSON.stringify(data, null, 2);
+  var lines = text.split("\n");
 
-export {
-    render_function
-}
+  for (var i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], 10, 20 + i * 20);
+  }
+  // eslint-disable-next-line no-undef
+  window.render_completed();
+};
+
+export { render_function };
